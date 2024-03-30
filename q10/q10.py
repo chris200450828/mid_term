799f14a37037f5d85e79fb5ed9c72421bebe575b
64_Xi <81499017+chris200450828@users.noreply.github.com>
2024-03-30 23:14:51 +0800

    Update q10.py
    file name manager!

--- q10/q10.py
+++ q10/q10.py
@@ -1,5 +1,7 @@
 import q10_command_stuff as qcf
 
+
 qcf.day_manager(opt=True)
 qcf.advance_link_mode(opt=False, pop=True)
-qcf.get_data()
+data_list = qcf.get_data()
+qcf.advance_file_manager(data_list, opt=False)
