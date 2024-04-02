63a26b7c00b9b55a576298d9d1f0d9492c1869f9
64_Xi <81499017+chris200450828@users.noreply.github.com>
2024-04-03 01:06:21 +0800

    Update q9.py

--- q9/q9.py
+++ q9/q9.py
@@ -21,3 +21,26 @@ if file_exist_checker():
     df = pd.DataFrame(data, columns=['Name', 'Release_Date', 'URL'])
     df.to_csv('movie.csv', index=False, encoding=encoding)
     print("process success")
+import q9_command_stuff as cf
+import pandas as pd
+import os
+
+cf.get_release()
+cf.link_mix()
+data = cf.get_release_data()
+
+
+def file_exist_checker():
+    file = "movie.csv"
+    if os.path.isfile(file):
+        print("File exist,process failed")
+        return False
+    else:
+        return True
+
+
+encoding = 'utf-8-sig'
+if file_exist_checker():
+    df = pd.DataFrame(data, columns=['Name', 'Release_Date', 'URL'])
+    df.to_csv('movie.csv', index=False, encoding=encoding)
+    print("process success")
