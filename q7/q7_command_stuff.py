5147bf6c06e8853644cffac40734f8553a8e57f2
64_Xi <81499017+chris200450828@users.noreply.github.com>
2024-04-11 09:28:14 +0800

    Update q7_command_stuff.py

--- q7/q7_command_stuff.py
+++ q7/q7_command_stuff.py
@@ -1,71 +1,33 @@
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-import re
+import pandas as pd
+import q7_command_stuff as qcs
 
-general_debug = False
+result_list = []
+TEAM = ["CLE", "HOU", "GSW"]
+num_l, name_l, pos_l, ht_l, wt_l, exp_l, college_l, birth_l = [], [], [], [], [], [], [], []
+
+url = qcs.start_up(TEAM)
+for i in url:
+    print(i)
+    soup = qcs.get_soup(i)
+    qcs.get_data(soup)
+    result_list.append(qcs.get_data(soup))
+
+#    解壓縮重新封裝zip
+for i in result_list:
+    for j in i:
+        obj = list(j)
+        num_l.append(obj[0])
+        name_l.append(obj[1])
+        pos_l.append(obj[2])
+        ht_l.append(obj[3])
+        wt_l.append(obj[4])
+        exp_l.append(obj[5])
+        college_l.append(obj[6])
+        birth_l.append(obj[7])
 
 
-def start_up(team):
-    url_List = []
-    url_front = "https://www.basketball-reference.com/teams/"
-    url_back = "/2024.html"
-    for i in team:
-        url_result = url_front + i + url_back
-        url_List.append(url_result)
+result = zip(num_l, name_l, pos_l, ht_l, wt_l, exp_l, college_l, birth_l)
 
-    return url_List
-
-
-def get_soup(target_url):
-    response = requests.get(target_url)
-    response.encoding = 'utf-8'
-    html = response.text
-    soup = BeautifulSoup(html, 'lxml')
-
-    return soup
-
-
-def get_data(soup):
-    num_l, name_l, pos_l, ht_l, wt_l, exp_l, college_l, birth_l = [], [], [], [], [], [], [], []
-    i = 0
-
-    table_parent = soup.find("table", class_="sortable")
-    table = table_parent.find("tbody")
-    data = table.find_all("tr")
-    for i in data:
-        num = i.find("th", {'data-stat': "number"})
-        if num.text == '' or num.text is None:
-            num_l.append("NULL")
-        else:
-            num_l.append(num.text)
-
-        name_grand = i.find("td", {'data-stat': "player"})
-        name_parent = name_grand.find("a")
-        name_l.append(name_parent.text)
-
-        pos = i.find("td", {'data-stat': "pos"})
-        pos_l.append(pos.text)
-
-        ht = i.find("td", {'data-stat': "height"})
-        ht_l.append(ht.text)
-
-        wt = i.find("td", {'data-stat': "weight"})
-        wt_l.append(wt.text)
-
-        exp = i.find("td", {'data-stat': "years_experience"})
-        exp_l.append(exp.text)
-
-        try:
-            college_parent = i.find("td", {'data-stat': "college"})
-            college = college_parent.find('a')
-            college_l.append(college.text)
-        except AttributeError as double_layer_elemnet:
-            college = 'NULL'
-            college_l.append(college)
-
-        birth = i.find("td", {'data-stat': "birth_date"})
-        birth_l.append(birth.text)
-
-    result = zip(num_l, name_l, pos_l, ht_l, wt_l, exp_l, college_l, birth_l)
-    return result
+encodings = 'utf-8-sig'
+df = pd.DataFrame(result, columns=['No.', 'Name', 'Position', 'Height', 'Weight', 'Experience', 'College', 'Birth'])
+df.to_csv('player.csv', index=False, encoding=encodings)
