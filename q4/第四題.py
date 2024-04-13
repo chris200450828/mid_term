7d3765ccbf98a18073ece330870cead10ef1a93b
cleverfishfishfish <162530106+cleverfishfishfish@users.noreply.github.com>
2024-04-13 23:49:09 +0800

    Add files via upload

---
+++ q4/第四題.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+requests.packages.urllib3.disable_warnings()
+
+url = "https://od.cdc.gov.tw/eic/NHI_EnteroviralInfection.csv"
+response = requests.get(url, verify=False)
+
+with open("NHI_EnteroviralInfection.csv", 'w', encoding='utf-8') as csvfile:
+    csvfile.write(response.content.decode('utf-8'))
+
+
+print("CSV文件已成功写入。")
