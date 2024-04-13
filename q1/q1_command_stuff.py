fa63bd37585a6ef409b8c4e515870582babc5151
64_Xi <81499017+chris200450828@users.noreply.github.com>
2024-04-13 22:56:16 +0800

    Create q1_command_stuff.py

---
+++ q1/q1_command_stuff.py
@@ -0,0 +1,31 @@
+class FuncsStuff:
+    def __init__(self, soup):
+        self.soup = soup
+
+    def _get_data(self):
+        dura_l, title_l, desc_l, link_l = [], [], [], []
+
+        parent_tag = self.soup.find_all('article', class_='filmList')
+        for i in parent_tag:
+            link = i.find('div', class_="filmTitle").find('a')
+            title = i.find('div', class_='filmTitle').find('a')
+            link_l.append(link.get('href'))
+            title_l.append(title.string)
+
+            desc = i.find('p')
+            desc_l.append(desc.string)
+
+            dura = i.find('div', class_="runtime")
+            dura_l.append(dura.text.strip('  上映日期：4/12/2024  上映廳數 (9)'))
+
+        return dura_l, title_l, desc_l, link_l
+
+    def print_out(self):
+        dura, title, desc, link = self._get_data()
+        loop_time = len(dura)
+        for i in range(loop_time):
+            print(title[i])
+            print(link[i])
+            print(desc[i])
+            print(dura[i])
+            print('----------------------------------------------------')
