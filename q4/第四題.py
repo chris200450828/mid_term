004ec0696f7c36ccec950216f785a60e7c6646c9
cleverfishfishfish <162530106+cleverfishfishfish@users.noreply.github.com>
2024-04-14 16:56:52 +0800

    Update 第四題.py

--- q4/第四題.py
+++ q4/第四題.py
@@ -7,8 +7,8 @@ requests.packages.urllib3.disable_warnings()
 url = "https://od.cdc.gov.tw/eic/NHI_EnteroviralInfection.csv"
 response = requests.get(url, verify=False)
 
-with open("NHI_EnteroviralInfection.csv", 'w', encoding='utf-8') as csvfile:
-    csvfile.write(response.content.decode('utf-8'))
+with open("NHI_EnteroviralInfection.csv", 'w', encoding='utf-8-sig') as csvfile:
+    csvfile.write(response.content.decode('utf-8-sig'))
 
 
 print("CSV文件已成功写入。")
