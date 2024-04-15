51c804a3859bb82e671bc8c0ecf102546f485ef7
64_Xi <81499017+chris200450828@users.noreply.github.com>
2024-04-15 22:40:59 +0800

    Update q2_command_stuff.py

--- q2/q2_command_stuff.py
+++ q2/q2_command_stuff.py
@@ -6,25 +6,29 @@ class FuncStuff:
         link_l, title_l, author_l = [], [], []
         grand = self.soup.find_all('div', class_="r-ent")
         for i in grand:
-            tag_title = i.find('div', class_="title").find('a')
-            link = tag_title.get('href')
-            author = i.find("div", class_='meta').find("div", class_='author')
-            if tag_title.string:
+            try:
+                tag_title = i.find('div', class_="title").find('a')
+                title_l.append(tag_title.string)
+            except AttributeError as DeletedObject:
+                tag_title = i.find('div', class_="title")
+                title_l.append(tag_title.string)
+            try:
+                link = tag_title.get('href')
                 link_l.append(link)
-                title_l.append(tag_title.string)
-            else:
-                link_l.append('Deleted')
-                title_l.append(tag_title.string)
+            except AttributeError as DeletedObject:
+                link = "Deleted"
+                link_l.append(link)
+            author = i.find("div", class_='meta').find("div", class_='author')
             author_l.append(author.string)
 
         return link_l, title_l, author_l
 
     def print_out(self):
-        link, time, author = self._get_data_list()
-        loop_time = len(link)
+        link, title, author = self._get_data_list()
+        loop_time = len(author)
 
         for i in range(loop_time):
             print(link[i])
-            print(time[i])
+            print(title[i])
             print(author[i])
             print("----------------------------------------------")
