6cc4a2df5ed8d83d0a8c5128d0cdf496020363e8
64_Xi <81499017+chris200450828@users.noreply.github.com>
2024-04-17 20:41:01 +0800

    Update q3_command_stuff.py

--- q3/q3_command_stuff.py
+++ q3/q3_command_stuff.py
@@ -7,10 +7,10 @@ class FuncStart:
         self.soup = soup
         self.history_soup = history_soup
 
-    def _get_data(self):
+    def _get_data(self):    ##抓取資料函式,通常而言不會在main裡call到這個函式
         currency_l, cash_buy_l, cash_sell_l, spot_buy_l, spot_sell_l = [], [], [], [], []
-        table = self.soup.find("table", {"title": "牌告匯率"}).find("tbody")
-        for row in table.find_all("tr"):
+        table = self.soup.find("table", {"title": "牌告匯率"}).find("tbody")    #抓取最上層的標籤
+        for row in table.find_all("tr"):    #迴圈遍歷,以下為資料抓取
             cells = row.find_all("td")
 
             currency = cells[0].text.strip()
@@ -25,10 +25,10 @@ class FuncStart:
             spot_buy_l.append(spot_buy)
             spot_sell_l.append(spot_sell)
 
-        return currency_l, cash_buy_l, cash_sell_l, spot_buy_l, spot_sell_l
+        return currency_l, cash_buy_l, cash_sell_l, spot_buy_l, spot_sell_l    #回傳抓到的變數
 
-    def print_out(self):
-        currency_l, cash_buy_l, cash_sell_l, spot_buy_l, spot_sell_l = self._get_data()
+    def print_out(self):    #打印出變數
+        currency_l, cash_buy_l, cash_sell_l, spot_buy_l, spot_sell_l = self._get_data()     #於print_out內部呼叫_get_data
         loop_time = len(currency_l)
         for i in range(loop_time):
             print(currency_l[i])
@@ -37,11 +37,11 @@ class FuncStart:
             print(spot_buy_l[i])
             print(spot_sell_l[i])
 
-    def _history_data(self):
-        table = self.history_soup.find("table", {"title": "歷史匯率收盤價"}).find("tbody")
+    def _history_data(self):    #歷史匯率的資料抓取,最上層命名不一樣導致要不同的寫法
+        table = self.history_soup.find("table", {"title": "歷史匯率收盤價"}).find("tbody")    #抓取__init__內部的歷史匯率的soup
         currency_l, cash_buy_l, cash_sell_l, spot_buy_l, spot_sell_l = [], [], [], [], []
 
-        for row in table.find_all("tr"):
+        for row in table.find_all("tr"):    #迴圈遍歷,以下為資料抓取
             cells = row.find_all("td")
 
             currency = cells[0].text.strip()
@@ -58,7 +58,7 @@ class FuncStart:
 
         return currency_l, cash_buy_l, cash_sell_l, spot_buy_l, spot_sell_l
 
-    def print_out_history(self):
+    def print_out_history(self):    #打印出變數
         currency_l, cash_buy_l, cash_sell_l, spot_buy_l, spot_sell_l = self._history_data()
         loop_time = len(currency_l)
         for i in range(loop_time):
@@ -68,28 +68,28 @@ class FuncStart:
             print(spot_buy_l[i])
             print(spot_sell_l[i])
 
-    def get_forward_url(self):
+    def get_forward_url(self):    #抓取遠期匯率的連結
         table = self.soup.find("table", {"title": "牌告匯率"}).find("tbody")
         forward_l = []
-        for row in table.find_all("tr"):
+        for row in table.find_all("tr"):    #迴圈遍歷,以下為資料抓取
             cells = row.find_all("td")
             forward = cells[5].find('a')
             forward_l.append(forward.get('href'))
         return forward_l
 
-    def dealed_url(self):    #    返回遠期匯率的url
+    def dealed_url(self):    #處理並返回正確的遠期匯率的url
         base_url = "https://rate.bot.com.tw/xrt?Lang=zh-TW"
-        forward_l = self.get_forward_url()
+        forward_l = self.get_forward_url() #
         result_forward = []
         for forward in forward_l:
             result_forward.append(urljoin(base_url, forward))
 
         return result_forward
 
-    def get_forward_data(self, forward_soup):    #    僅接受一個soup   
+    def get_forward_data(self, forward_soup):    #僅接受一個soup,為遠期匯率的soup,通常由main裡迴圈依次傳遞
         date_l, buy_l, sell_l = [], [], []
         forward_soup = forward_soup
-        title = forward_soup.find('th', class_='currency').find('div').find('div').find('div', class_='hidden-phone')
+        title = forward_soup.find('th', class_='currency').find('div').find('div').find('div', class_='hidden-phone')    #以下皆為資料抓取
         table_rows = forward_soup.find("table", {"title": "遠期匯率"}).find("tbody").find_all('tr')
         for i in table_rows:
             cells = i.find_all('td')
@@ -101,9 +101,9 @@ class FuncStart:
             buy_l.append(buy_in)
             sell_l.append(sell_out)
 
-        return date_l, buy_l, sell_l
+        return date_l, buy_l, sell_l    #返回遠期匯率的資料
 
-    def save_csv(self, opt=None):
+    def save_csv(self, opt=None):    #opt為選項,如果為真會將歷史匯率跟當期匯率的資料分開儲存成不同的csv,若為否則一塊儲存
         currency_l, cash_buy_l, cash_sell_l, spot_buy_l, spot_sell_l = self._get_data()
         h_currency_l, h_cash_buy_l, h_cash_sell_l, h_spot_buy_l, h_spot_sell_l = self._history_data()
         if opt is True:
@@ -135,7 +135,7 @@ class FuncStart:
 
             df.to_csv('bank', index=False, encoding='utf-8-sig')
 
-    def to_forward_csv(self, filename, result):
+    def to_forward_csv(self, filename, result):    #儲存遠期匯率的csv,由於幣值太多,儲存於同一個csv內會極度不美觀,所以採用分開儲存的方式
         result = result
         df = pandas.DataFrame(result, columns=['時間', '本行買入匯率', '本行賣出匯率'])
         filename = filename + '_forward_rate.csv'
