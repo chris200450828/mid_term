ae77d3d575fa468ceda83c1ebd48843486488a46
64_Xi <81499017+chris200450828@users.noreply.github.com>
2024-04-17 20:58:31 +0800

    Update q8.py

--- q8/q8.py
+++ q8/q8.py
@@ -5,17 +5,17 @@ import pandas as pd
 url = "https://www.googleapis.com/books/v1/volumes"
 url_params = {'q': 'Python', 'maxResults': 10, 'projection': 'lite'}
 
-r = requests.get(url, params=url_params)
+r = requests.get(url, params=url_params)  #請求api
 data = r.json()
-title, author, link, puber, pubDate = qcs.get_data(data, pop=False)
+title, author, link, puber, pubDate = qcs.get_data(data, pop=False)  #處理請求後的資料
 
-url_params['startIndex'] = 10
+url_params['startIndex'] = 10  #設定第二頁要用到的參數
 
-next_r = requests.get(url, params=url_params)
+next_r = requests.get(url, params=url_params)  #請求api
 next_data = next_r.json()
-n_title, n_authors, n_link, n_puber, n_pubDate = qcs.get_data(next_data, pop=False)
+n_title, n_authors, n_link, n_puber, n_pubDate = qcs.get_data(next_data, pop=False)  #處理請求後的資料
 
-result_title = qcs.merge(title, n_title)
+result_title = qcs.merge(title, n_title)  #混合函式發揮作用
 result_author = qcs.merge(author, n_authors)
 result_link = qcs.merge(link, n_link)
 result_puber = qcs.merge(puber, n_puber)
